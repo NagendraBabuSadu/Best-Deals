7ca34b16858bd2d1232bee840373d9e3fdc8811c
NagendraBabuSadu <89573050+NagendraBabuSadu@users.noreply.github.com>
2025-02-27 10:53:02 +0530

    Done small changes

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -4,9 +4,13 @@ import "./index.css";
 import App from "./App.jsx";
 import { ThemeProvider } from "@emotion/react";
 import theme from "./theme.js";
+import store from "./app/store.js";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </Provider>
 );
